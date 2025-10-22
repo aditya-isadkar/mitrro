a77fd9dc7e27d5ecbf4ed8d3dbbb873aa054a0c4
Aditya <adityaisadkar940@gmail.com>
2025-10-22 19:29:39 +0530

    Added Google Analytics tracking code

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import FeaturedProducts from "@/components/FeaturedProducts";
@@ -6,6 +7,19 @@ import Footer from "@/components/Footer";
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QE6BLPV4K8"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-QE6BLPV4K8');
+          `}
+        </script>
+      </Head>
+      
       <Header />
       <main>
         <Hero />
